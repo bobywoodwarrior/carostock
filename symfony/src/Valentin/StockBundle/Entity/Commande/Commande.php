4065f23e8a721fc04baa08d81b852dbdfe9f806c
dubosc_a <valentin1.duboscq@epitech.eu>
2015-07-07 10:01:08 +0200

    bdd

---
+++ symfony/src/Valentin/StockBundle/Entity/Commande/Commande.php
@@ -0,0 +1,52 @@
+<?php
+namespace Valentin\StockBundle\Entity\Commande;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Commande
+ *
+ * @ORM\Table(name="commande")
+ * @ORM\Entity()
+ */
+class Commande
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="nombre", type="datetime")
+     */
+    protected $nombre;
+
+    /**
+     * @var datetime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    protected $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    protected $name;//ref ou nom ?
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->nombre = 1;
+    }
+}
+
