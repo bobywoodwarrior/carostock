c50d244d72ad101702e6a3728f0c466a88787592
dubosc_a <valentin1.duboscq@epitech.eu>
2015-09-09 16:08:33 +0200

    edit+delete

--- symfony/src/Valentin/StockBundle/Controller/ProductController.php
+++ symfony/src/Valentin/StockBundle/Controller/ProductController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace Valentin\StockBundle\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,7 +12,6 @@ use Valentin\StockBundle\Form\ProductType;
  * Date: 04/09/15
  * Time: 17:46
  */
-
 /**
  * @Route("/product")
  */
@@ -31,38 +28,98 @@ class ProductController extends Controller
             ->getRepository('ValentinStockBundle:Product')
             ->findAll()
         ;
-
         return $this->render('ValentinStockBundle:Product:index.html.twig', array(
             'products' => $products
         ));
     }
-<<<<<<< HEAD
-=======
 
     /**
-     * Product Index
+     * Product Add
      *
      * @Route("/add", name="product_add")
      */
->>>>>>> ef80f04c1f1af742b5ffaf16caa56739e55851f4
     public function addProduct(Request $request)
     {
         $product = new Product();
-
         $form = $this->createForm(new ProductType(), $product);
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($product);
+                $em->flush();
+                $this->get('session')->getFlashBag()->add(
+                    'success',
+                    'Produit ajouté!'
+                );
+                return $this->redirect(
+                    $this->generateUrl(
+                        'product_index',
+                        array(
+                            'id' => $product->getId()
+                        )
+                    )
+                );
+            }
+        }
+        return $this->render('ValentinStockBundle:Product:add.html.twig', array(
+            'product' => $product,
+            'form'    => $form->createView()
+        ));
+    }
+
+    /**
+     * Product Delete
+     *
+     * @Route("/delete", name="product_delete")
+     * @param Product $product
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function deleteAction(Request $request, Product $product)
+    {
+        if ($request->getMethod() === 'POST'){
+            $em = $this->getDoctrine()->getManager();
+
+            $em->remove($product);
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->add(
+                'success',
+                'Produit supprimé!'
+            );
+
+            return $this->redirect(
+                $this->generateUrl(
+                    'product_index'
+                )
+            );
+        }
+
+        return $this->render('ValentinStockBundle:Product:delete.html.twig', array(
+            'product' => $product
+        ));
+    }
+
+    /**
+     * Product Edit
+     *
+     * @param Product $product
+     * @Route("/edit", name="product_edit")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function editAction(Request $request, Product $product)
+    {
+        $form = $this->createForm(new Product(), $product);
 
         if ($request->isMethod('POST')) {
             $form->handleRequest($request);
             if ($form->isValid()) {
-
                 $em = $this->getDoctrine()->getManager();
 
-                $em->persist($product);
                 $em->flush();
-
                 $this->get('session')->getFlashBag()->add(
                     'success',
-                    'Produit ajouté!'
+                    'Changements sauvegardés!'
                 );
 
                 return $this->redirect(
@@ -76,9 +133,9 @@ class ProductController extends Controller
             }
         }
 
-        return $this->render('ValentinStockBundle:Product:add.html.twig', array(
-            'product' => $product,
-            'form'    => $form->createView()
+        return $this->render('@OvinzDevis/Backend/Affiliate/edit.html.twig', array(
+            'affiliate' => $product,
+            'form'      => $form->createView()
         ));
     }
 }
