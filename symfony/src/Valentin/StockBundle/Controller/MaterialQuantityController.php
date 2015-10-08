8ac0e2de8462134a3a81e49d098589a419ba8c9a
dubosc_a <valentin1.duboscq@epitech.eu>
2015-10-08 17:17:32 +0200

    MaterialQuantity

---
+++ symfony/src/Valentin/StockBundle/Controller/MaterialQuantityController.php
@@ -0,0 +1,139 @@
+<?php
+namespace Valentin\StockBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Valentin\StockBundle\Entity\MaterialQuantity;
+use Valentin\StockBundle\Form\MaterialQuantityType;
+
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 04/09/15
+ * Time: 17:46
+ */
+/**
+ * @Route("/material_quantity")
+ */
+class MaterialQuantityController extends Controller
+{
+    /**
+     * Material Kind Index
+     *
+     * @Route("/", name="material_quantity_index")
+     */
+    public function indexAction()
+    {
+        $quantities = $this->getDoctrine()->getManager()
+            ->getRepository('ValentinStockBundle:MaterialQuantity')
+            ->findAll()
+        ;
+        return $this->render('ValentinStockBundle:MaterialQuantity:index.html.twig', array(
+            'quantities' => $quantities
+        ));
+    }
+
+    /**
+     * New Material Quantity
+     *
+     * @Route("/new", name="material_quantity_new")
+     */
+    public function newMaterialQuantity(Request $request)
+    {
+        $quantity = new MaterialQuantity();
+
+        $form = $this->createForm(new MaterialQuantityType(), $quantity);
+
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($quantity);
+                $em->flush();
+                $this->get('session')->getFlashBag()->add(
+                    'success',
+                    'Quantity added !'
+                );
+                return $this->redirect(
+                    $this->generateUrl(
+                        'material_quantity_index'
+                    )
+                );
+            }
+        }
+
+        return $this->render('ValentinStockBundle:MaterialQuantity:new.html.twig', array(
+            'form'     => $form->createView()
+        ));
+    }
+
+    /**
+     * Material Quantity Edit
+     *
+     * @param MaterialQuantity $quantity
+     * @Route("/edit/{id}", name="material_quantity_edit")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function editMaterialQuantity(Request $request, MaterialQuantity $quantity)
+    {
+        $form = $this->createForm(new MaterialQuantityType(), $quantity);
+
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+
+                $em->flush();
+                $this->get('session')->getFlashBag()->add(
+                    'success',
+                    'Quantity edited !'
+                );
+
+                return $this->redirect(
+                    $this->generateUrl(
+                        'material_quantity_index'
+                    )
+                );
+            }
+        }
+
+        return $this->render('ValentinStockBundle:MaterialQuantity:edit.html.twig', array(
+            'quantity' => $quantity,
+            'form'    => $form->createView()
+        ));
+    }
+
+    /**
+     * Material Quantity Delete
+     *
+     * @Route("/delete/{id}", name="material_quantity_delete")
+     * @param MaterialQuantity $quantity
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function deleteMaterialQuantity(Request $request, MaterialQuantity $quantity)
+    {
+        if ($request->getMethod() === 'POST'){
+            $em = $this->getDoctrine()->getManager();
+
+            $em->remove($quantity);
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->add(
+                'success',
+                'Quantity deleted !'
+            );
+
+            return $this->redirect(
+                $this->generateUrl(
+                    'material_quantity_index'
+                )
+            );
+        }
+
+        return $this->render('ValentinStockBundle:MaterialQuantity:delete.html.twig', array(
+            'quantity' => $quantity
+        ));
+    }
+
+}
---
+++ symfony/src/Valentin/StockBundle/Form/MaterialQuantityType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Valentin\StockBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class MaterialQuantityType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('quantity','number')
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Valentin\StockBundle\Entity\MaterialQuantity'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'valentin_stockbundle_material_quantity';
+    }
+}
